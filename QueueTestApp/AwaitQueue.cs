08a58945d65d371f6e458e693a7edae6ef4b1c4e
Дмитрий Санососюк <smittych@mail.ru>
2018-05-07 20:04:13 +0300

    Add project files.

---
+++ QueueTestApp/AwaitQueue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QueueTestApp
+{
+    //объект очереди с ожиданием элемента при пустой очереди
+    public class AwaitQueue<T>
+    {
+        //блокировка ожидания хотя бы одного элемента
+        private object waitLocker = new object();
+
+        //собственно, сам контейнер очереди
+        private Queue<T> queue = new Queue<T>();
+
+        //свойство, возвращающее список элементов очереди (для отображения на форме)
+        public List<T> QueueList
+        {
+            get
+            {
+                List<T> res = new List<T>();
+
+                foreach (T item in queue)
+                    res.Add(item);
+
+                return res;
+            }
+        }
+
+        //операция добавления элемента в очередь
+        public void push(T value)
+        {
+            lock (waitLocker)
+            {
+                queue.Enqueue(value);
+                Monitor.Pulse(waitLocker);
+            }
+        }
+
+        //операция изъятия элеменнта из очереди
+        public T pop()
+        {
+            T res = default(T);
+
+            lock (waitLocker)
+            {
+                if (queue.Count == 0)
+                    Monitor.Wait(waitLocker);
+                if (queue.Count != 0)
+                    res = queue.Dequeue();
+            }
+
+            return res;
+        }
+    }
+}
---
+++ QueueTestApp/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace QueueTestApp
+{
+    public partial class MainWindow : Window
+    {
+        static AwaitQueue<int> aQueue = new AwaitQueue<int>();  
+        private int pushIdx;  //инкрементируемый id объекта очереди (используется как сам объект очереди)
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPush_Click(object sender, RoutedEventArgs e)
+        {
+            //создание отдельного потока для push 
+            var threadPush = new Thread(Push);
+            threadPush.Start(pushIdx);
+
+            pushIdx++;
+        }
+
+        private void Push(object pushItem)
+        {
+            aQueue.push((int)pushItem);
+
+            Action action = () =>
+            {
+                FillPushList();
+            };
+            Dispatcher.Invoke(action);
+        }
+
+        private void btnPop_Click(object sender, RoutedEventArgs e)
+        {
+            //создание отдельного потока для pop 
+            var threadPop = new Thread(Pop);
+            threadPop.Start();
+        }
+
+        private void Pop()
+        {
+            var a = aQueue.pop();
+
+            Action action = () =>
+            {
+                PopList.Items.Add(a);
+                FillPushList();
+            };
+            Dispatcher.Invoke(action);
+        }
+
+        //отображение списка элеметнов очереди
+        public void FillPushList()
+        {
+            PushList.Items.Clear();
+            List<int> queueList = aQueue.QueueList;
+
+            for (int i = 0; i < queueList.Count; i++)
+            {
+                PushList.Items.Add(queueList[i]);
+            }
+        }
+    }
+}
